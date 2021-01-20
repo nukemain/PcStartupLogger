57e4a68d2ab119125c618a53e7cfc249434bef74
nukemain <sowa.piotr15@gmail.com>
2021-01-20 11:10:34 +0100

    Update PcStartupLogger.py
    Removed useless code

--- PcStartupLogger.py
+++ PcStartupLogger.py
@@ -1,13 +1,7 @@
-import time
 import datetime
-import calendar  
 import os        
-import msvcrt  
-import random
-import webbrowser
 import datetime
 import sys 
-from os.path import join
 from pathlib import Path
 
 home = str(Path.home())
@@ -15,8 +9,6 @@ path_to_file = str(home) + "\\PcStartupLogger.txt"
 File_Didnt_Exist_Until_Now = False
 try: 
     logfile = open(path_to_file,'x') #opening a file with option x essentialy just checks if it exists
-    print("flag-1")                                                                                             #flag-1
-    #logfile.close() #the above function actually opens the file so we need to close it
     File_Didnt_Exist_Until_Now  = True
 except FileExistsError: 
     print("FileExistsError")
@@ -25,10 +17,8 @@ logfile = open(path_to_file,'a')
 if(File_Didnt_Exist_Until_Now):
     logfile.write("Pc turned on @ "+str(datetime.datetime.now())+" . Times this PC has been turned on since this files creation: 1 \n")
     logfile.close()
-    time.sleep(5)
     sys.exit()
 else:
-    print("flag-2")
     logfile.close()
     logfile = open(path_to_file,'r')
     contents_of_the_log = logfile.readlines()
@@ -40,5 +30,4 @@ else:
     logfile = open(path_to_file,'a')
     logfile.write("Pc turned on @ "+str(datetime.datetime.now())+" . Times this PC has been turned on since this files creation: "+str(New_turned_on_counter)+" \n")
     logfile.close()
-    time.sleep(5)
     sys.exit()
